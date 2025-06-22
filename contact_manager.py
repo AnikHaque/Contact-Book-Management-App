8e03adda17a0c0f3b2f5962b68674727c87de8db
AnikHaque <ekramul15-811@diu.edu.bd>
2025-06-22 22:01:06 +0600

    search_contacts

---
+++ contact_manager.py
@@ -0,0 +1,29 @@
+from utils import is_valid_name, is_valid_phone, is_valid_email
+
+class ContactManager:
+    def __init__(self):
+        self.contacts = []
+
+    def load_contacts(self, data):
+        self.contacts = data
+
+    def get_contacts(self):
+        return self.contacts
+
+    def add_contact(self, contact):
+        for c in self.contacts:
+            if c['phone'] == contact['phone']:
+                return False, "Error: Phone number already exists for another contact."
+        self.contacts.append(contact)
+        return True, "Contact added successfully!"
+
+
+def search_contacts(self, term):
+        result = []
+        term = term.lower()
+        for contact in self.contacts:
+            if (term in contact['name'].lower() or
+                term in contact['email'].lower() or
+                term in contact['phone']):
+                result.append(contact)
+        return result
